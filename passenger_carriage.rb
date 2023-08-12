862e4e3346e7620743c0f416cfc26ebe503d103c
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-08-12 12:15:46 +0300

    Add seats and free_seats attributes Add methods to take seat, release seat and return free seats Add validation of seats attribute

--- passenger_carriage.rb
+++ passenger_carriage.rb
@@ -1,12 +1,37 @@
-class PassengerCarriage
+# Добавить атрибут общего кол-ва мест (задается при создании вагона)
+# Добавить метод, который "занимает места" в вагоне (по одному за раз)
+# Добавить метод, который возвращает кол-во занятых мест в вагоне
+# Добавить метод, возвращающий кол-во свободных мест в вагоне.
+
+class PassengerCarriage < Carriage
   include Manufacturer
 
-  attr_reader :type
+  attr_accessor :seats
+  attr_reader :type, :free_seats
 
   TYPE = :passenger
 
-  def initialize
-    super
-    @type = TYPE
+  def initialize(number_of_seats)
+    super()
+    @seats = number_of_seats.to_i
+    validate!
+    @free_seats = number_of_seats.to_i
+  end
+
+  def take_seat
+    @free_seats -= 1 unless free_seats.zero?
+  end
+
+  def release_seat
+    @free_seat += 1 if free_seats < seats
+  end
+
+  private
+
+  def validate!
+    if seats.negative?
+      puts 'Количество мест в вагоне не может быть отрицательным.'
+      raise RuntimeError
+    end
   end
 end
